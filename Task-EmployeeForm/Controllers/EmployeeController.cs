c6f30d490179b31bb2f43aaf4415805d7efabf51
Eman Alaa Khodary <emanalaakhodary@gmail.com>
2024-04-23 22:20:56 +0200

    Add project files.

---
+++ Task-EmployeeForm/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task_EmployeeForm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        []
+    }
+}
